856b9398c7a7d65d7273c8b7869b70ba85bd6339
Emirhan Oğul <95977203+ogul1@users.noreply.github.com>
2022-11-03 19:31:16 +0300

    Update btester.cpp

--- btester.cpp
+++ btester.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     const int tc = 200;
     int passed = 0;
-    char compile[50], compare[50], expected[50], sed1[80], sed2[80], failure[50];;
+    char compile[50], compare[50], expected[50], sed1[80], sed2[80], failure[50];
 
     for (int i = 1; i <= tc; ++i) {
 
@@ -14,21 +14,6 @@ int main() {
         
         system("./a.out > g.txt");
 
-        system("sed -i '/^Deallocated/d' g.txt");
-        system("sed -i '/^Allocated/d' g.txt");
-
-        // system("sed -i '' '/^Deallocated/d' g.txt"); // uncomment if mac
-        // system("sed -i '' '/^Allocated/d' g.txt"); // and comment the other one
-
-        snprintf(sed1, 80, "sed -i '/^Deallocated/d' window_out/output%d.txt", i);
-        snprintf(sed2, 80, "sed -i '/^Allocated/d' window_out/output%d.txt", i);
-
-        // snprintf(sed1, 80, "sed -i '' '/^Deallocated/d' browser_out/output%d.txt", i);
-        // snprintf(sed2, 80, "sed -i '' '/^Allocated/d' browser_out/output%d.txt", i); // uncomment if mac
-
-        system(sed1);
-        system(sed2);
-
         snprintf(compare, 50, "cmp -s browser_out/output%d.txt g.txt", i);
 
         if (!system(compare)) {
@@ -36,7 +21,7 @@ int main() {
             cout << "Case " << i << " PASSED.\n" << std::flush;
         } else {
             cout << "----------------------------------\n" << flush;
-            cout << "Case " << i << " FAILED.\n" << std::flush;
+            cout << "Case " << i << " FAILED.\n" << flush;
             cout << "Given Output:\n" << flush;
             system("cat g.txt");
             cout << "Expected Output:\n" << flush;
