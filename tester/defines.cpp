f17d3aacec6a2bd286b9d4ee940368eb8c33ccfc
Emirhan Oğul <95977203+ogul1@users.noreply.github.com>
2022-11-03 19:33:25 +0300

    Delete defines.cpp

--- tester/defines.cpp
+++
@@ -1,5 +0,0 @@
-#include <cstring>
-#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
-#define delete file = __FILENAME__, line = __LINE__, func = __func__, delete
-#define new new(__FILENAME__, __func__, __LINE__)
-
