32015c511a4a9df10658bacc8300a3918a2657a3
Emirhan Oğul <95977203+ogul1@users.noreply.github.com>
2022-11-03 19:30:42 +0300

    Update playground.cpp

--- playground.cpp
+++ playground.cpp
@@ -8,16 +8,9 @@
 #include "tester/Window.cpp"
 #include "tester/Browser.cpp"
 #include "tester/LinkedList.cpp"
-long long _Total_Memory_Deallocated;
-long long _Total_Memory_Allocated;
-long long line;
-std::string file;
-std::string func;
 
 int main() {
-    _Total_Memory_Deallocated = 0;
-    _Total_Memory_Allocated = 0;
-    line = 0;
+
 
     /*
      * you can test your implementations here...
@@ -26,10 +19,5 @@ int main() {
      *
      * good luck senpai...
      */
-
-
-    std::cout << "\n";
-    std::cout << "Total Memory Allocated: " << _Total_Memory_Allocated << " bytes.\n"
-              << "Total Memory Deallocated: " << _Total_Memory_Deallocated << " bytes.\n";
 	return 0;
 };
